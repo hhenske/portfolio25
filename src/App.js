0fcffb2f52d647ca7a920e9cded439ab8ad41ac0
Holly Henske <hcover333@gmail.com>
2025-10-01 13:17:48 -0500

    landing page roughed in and responsive

--- src/App.js
+++ src/App.js
@@ -16,17 +16,23 @@ function App() {
 
       <main className="landing">
         <section className="intro">
-          <h1>Designing & Building Digital Experiences</h1>
+          <h1>Digital Creator and Collaborator</h1>
           <p>
             I’m Holly, a Web Developer, UX Designer, and Bubble.io Developer. I
-            craft accessible and creative digital solutions.
+            craft accessible and user-centered digital solutions. Excellent collaborator and great listener. Let's create bring your vision to the screen!
           </p>
         </section>
 
         <section className="categories">
-          <div className="category">Web Developer</div>
-          <div className="category">UX Designer</div>
-          <div className="category">Bubble.io Developer</div>
+          <div className="category" id="web-dev-category">
+            <span className="category-label">Web Developer</span>
+            </div>
+          <div className="category" id="ux-design-category">
+            <span className="category-label">UX Designer</span>
+            </div>
+          <div className="category" id="bubble-dev-category">
+            <span className="category-label">Bubble.io Developer</span>
+            </div>
         </section>
       </main>
     </div>
